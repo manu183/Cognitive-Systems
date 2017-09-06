c8b8123a72790b1ece17f53f8c0ab33432fb7b4c
manu183 <manuellang183@gmail.com>
2017-09-06 16:14:13 +0200

    implemented filters

---
+++ filter.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def median(shape=(3,3)):
+	return np.ones(shape,np.float32)/(shape[0] * shape[1])
+
+def gauss_2d(shape=(3,3),sigma=0.5):
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m : m+1, -n : n+1]
+    h = np.exp( -(x * x + y * y) / (2. * sigma * sigma) )
+    h[ h < np.finfo(h.dtype).eps * h.max() ] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
+
+def prewitt_x():
+	return np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+
+def prewitt_y():
+	return np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+
+def sobel_x():
+	return np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+
+def sobel_y():
+	return np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+def roberts_x():
+	return np.array([[-1, 0], [0, 1]])
+
+def roberts_y():
+	return np.array([[0, -1], [1, 0]])
+
+def laplace():
+	return np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+
+def laplace_alt():
+	return np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]])
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
-import matplotlib.pyplot as plt
+import numpy as np
 from histogram import histogram_stretching, histogram_stretching_quantile, histogram_equalization
+from filter import gauss_2d, median, prewitt_x, prewitt_y, sobel_x, sobel_y, roberts_x, roberts_y, laplace, laplace_alt
 from os.path import basename, dirname, join, splitext
 
 def stretch_histogram(image_path):
@@ -10,7 +11,7 @@ def stretch_histogram(image_path):
 	im = cv2.imread(image_path) 
 	im_grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 	im_fixed_contrast = histogram_stretching(im_grey)
-	cv2.imwrite(join(folder, ''.join((filename + '-hist_stretched', extension))), im_fixed_contrast)
+	cv2.imwrite(join(folder, ''.join((filename, '-hist_stretched', extension))), im_fixed_contrast)
 
 def stretch_histogram_quantile(image_path):
 	filename = basename(splitext(image_path)[0])
@@ -19,7 +20,7 @@ def stretch_histogram_quantile(image_path):
 	im = cv2.imread(image_path) 
 	im_grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 	im_fixed_contrast = histogram_stretching_quantile(im_grey)
-	cv2.imwrite(join(folder, ''.join((filename + '-hist_stretched_quantile', extension))), im_fixed_contrast)
+	cv2.imwrite(join(folder, ''.join((filename, '-hist_stretched_quantile', extension))), im_fixed_contrast)
 
 def equalize_histogram(image_path):
 	filename = basename(splitext(image_path)[0])
@@ -28,8 +29,27 @@ def equalize_histogram(image_path):
 	im = cv2.imread(image_path) 
 	im_grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 	im_fixed_contrast = histogram_equalization(im_grey)
-	cv2.imwrite(join(folder, ''.join((filename + '-equalized_hist', extension))), im_fixed_contrast)
+	cv2.imwrite(join(folder, ''.join((filename, '-equalized_hist', extension))), im_fixed_contrast)
 
-stretch_histogram("imgs/family-bad_contrast.jpg")
-stretch_histogram_quantile("imgs/family-bad_contrast.jpg")
-equalize_histogram("imgs/family-bad_contrast.jpg")
+def discrete_fourier_transform(image_path):
+	filename = basename(splitext(image_path)[0])
+	extension = splitext(image_path)[1]
+	folder = dirname(splitext(image_path)[0])
+	im = cv2.imread(image_path, 0)
+	dft = cv2.dft(np.float32(im), flags = cv2.DFT_COMPLEX_OUTPUT)
+	dft_shift = np.fft.fftshift(dft)
+	im_ft = 20 * np.log(cv2.magnitude(dft_shift[:,:,0], dft_shift[:,:,1]))
+	cv2.imwrite(join(folder, ''.join((filename, '-fourier', extension))), im_ft)
+
+def convolve(image_path, filter):
+	filename = basename(splitext(image_path)[0])
+	extension = splitext(image_path)[1]
+	folder = dirname(splitext(image_path)[0])
+	im = cv2.imread(image_path, 0)
+	cv2.imwrite(join(folder, ''.join((filename, '-convolved', extension))), cv2.filter2D(im, -1, filter))
+
+# stretch_histogram("imgs/family-bad_contrast.jpg")
+# stretch_histogram_quantile("imgs/family-bad_contrast.jpg")
+# equalize_histogram("imgs/family-bad_contrast.jpg")
+# discrete_fourier_transform("imgs/family-bad_contrast.jpg")
+convolve("imgs/waterfall_jam.jpg", laplace_alt())
